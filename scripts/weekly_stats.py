d46e2e0a19eb953cb30e68c161213902361d4cdd
Michael Dereszynski <mlderes@hotmail.com>
2024-11-27 18:27:31 +0000

    Make an update to use the roster_coaches view

--- scripts/weekly_stats.py
+++ scripts/weekly_stats.py
@@ -38,19 +38,7 @@ if __name__ == '__main__':
     print(f'Updating matchups for week {latest_week}')
     update_weekly_matchups(latest_week, drop_table=False)
 
-    df_outcomes = run_query('''WITH roster_coaches AS (
-    SELECT 
-        r.roster_id, 
-        r.league_id,
-        l.name as league_name,
-        c.display_name as coach_name,
-        c.user_id
-    FROM 
-        rosters r JOIN coaches c ON r.owner_id = c.user_id and r.league_id = c.league_id
-        JOIN league l on r.league_id = l.league_id
-    GROUP BY 
-        r.roster_id, r.league_id, l.name, c.display_name, c.user_id -- Aggregate to one row per roster
-    )
+    df_outcomes = run_query('''
     SELECT r1.league_name, m1.week, r1.coach_name as 'Winner', r2.coach_name as 'Loser', m1.points, m2.points
         FROM matchups m1 
         JOIN matchups m2 on m1.matchup_id = m2.matchup_id and m1.week = m2.week and m1.league_id = m2.league_id and m1.roster_id != m2.roster_id
@@ -61,19 +49,7 @@ if __name__ == '__main__':
     # Calculate the median for each league
     df_median_points = run_query('''select league_id, week, points from matchups''').groupby(['league_id','week']).median().reset_index()
 
-    df_team_points = run_query('''WITH roster_coaches AS (
-        SELECT 
-            r.roster_id, 
-            r.league_id,
-            l.name as league_name,
-            c.display_name as coach_name,
-            c.user_id
-        FROM 
-            rosters r JOIN coaches c ON r.owner_id = c.user_id and r.league_id = c.league_id
-            JOIN league l on r.league_id = l.league_id
-        GROUP BY 
-            r.roster_id, r.league_id, l.name, c.display_name, c.user_id -- Aggregate to one row per roster
-    )
+    df_team_points = run_query('''
     SELECT r.league_id, r.league_name, m.week, m.roster_id, r.coach_name, m.points 
     FROM matchups m 
         JOIN roster_coaches r on m.roster_id = r.roster_id and m.league_id = r.league_id''')
