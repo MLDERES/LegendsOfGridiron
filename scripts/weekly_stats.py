c13b6b869aa64a5ac24d58bd6c447cddf615e4ff
Michael Dereszynski <mlderes@hotmail.com>
2024-12-02 18:05:23 +0000

    Create the standings each week.

--- scripts/weekly_stats.py
+++ scripts/weekly_stats.py
@@ -33,6 +33,42 @@ def update_weekly_matchups(week = 1, drop_table = False):
         
     conn.close()       
     
+def calculate_standings(df_results):
+    # Since we have all the results, let's calculate the standings
+    # The format of the file is League, Weeek, Winner, Loser, Winner Pts, Loser Pts
+    # First get the results by league
+    for l in df_results.League.unique():
+        df = df_results[df_results.League == l]
+        # Total the Points for and the points against by team, but also the points if they were the loser   
+        winner_points = df.groupby('Winner')['Winner Pts'].sum().reset_index().rename(columns={'Winner': 'Team', 'Winner Pts': 'Total Points'})
+        loser_points = df.groupby('Loser')['Loser Pts'].sum().reset_index().rename(columns={'Loser': 'Team', 'Loser Pts': 'Total Points'})
+        
+        # Combine the points from winners and losers
+        total_points_df = pd.concat([winner_points, loser_points]).groupby('Team').sum().reset_index().sort_values(by='Total Points', ascending=False)
+        total_points_df['Total Points'] = round(total_points_df['Total Points'],2)
+        # Now get the number of times each team has won in the league
+        df_wins = df.groupby('Winner').size().reset_index(name='Wins')
+        # Now get the number of times each team has lost in the league
+        df_losses = df.groupby('Loser').size().reset_index(name='Losses')
+        # combine the two dataframes
+        df_wins = df_wins.set_index('Winner')
+        df_losses = df_losses.set_index('Loser')
+        df_wins = df_wins.join(df_losses, how='outer')
+        # Join the total points
+        df_wins = df_wins.join(total_points_df.set_index('Team'), how='outer')
+        # fill in the missing values
+        df_wins = df_wins.fillna(0)
+        # calculate the win percentage
+        df_wins['Win_Percentage'] = round(df_wins['Wins'] / (df_wins['Wins'] + df_wins['Losses']),2)
+        # Sort by win percentage
+        df_wins = df_wins.sort_values('Win_Percentage', ascending=False)
+        # filter out "MEDIAN"
+        df_wins = df_wins[df_wins.index != 'MEDIAN']
+        
+        df_wins.to_csv(DATA_PATH/f'standings/league_{l}.csv', index=True, index_label='Team')
+    
+    
+    
 if __name__ == '__main__':
     latest_week = run_query_scalar("select max(week) from matchups")+1
     print(f'Updating matchups for week {latest_week}')
@@ -60,4 +96,8 @@ if __name__ == '__main__':
     df_combined['Winner']=df_combined.apply(lambda row: row['coach_name'] if row['points_team']> row['points_median'] else 'MEDIAN', axis=1)
     df_combined['Loser']=df_combined.apply(lambda row: row['coach_name'] if row['points_team']<= row['points_median'] else 'MEDIAN', axis=1)
     df_output= df_combined[['league_name','week','Winner','Loser']].rename(columns={'league_name':'League','week':'Week'})
-    pd.concat([df_outcomes,df_output], axis=0).to_csv(DATA_PATH/'league_outcomes.csv', index=False)
+    df_results = pd.concat([df_outcomes,df_output], axis=0)
+    df_results.to_csv(DATA_PATH/'league_outcomes.csv', index=False)
+    
+    # Now update the standings
+    calculate_standings(df_results)
